f8c8093009c5435ca1a95a234cd5611845795dac
CanadaHonk <oj@oojmed.com>
2023-09-19 00:03:06 +0100

    style: minor improvements

--- static/graph.js
+++ static/graph.js
@@ -1,6 +1,6 @@
 var svg = d3.select("svg"),
     margin = {top: 20, right: 80, bottom: 30, left: 50},
-    width = window.innerWidth - 8 - 40 - margin.left - margin.right, // svg.attr("width") - margin.left - margin.right,
+    width = window.innerWidth - 8 - 60 - margin.left - margin.right, // svg.attr("width") - margin.left - margin.right,
     height = svg.attr("height") - margin.top - margin.bottom,
     g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
